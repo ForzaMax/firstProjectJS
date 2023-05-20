d0e73f93563889f4225725bbf52d4765449c1e22
fforzamax <fforzamax@gmail.com>
2023-05-20 10:51:34 +0600

    first lesson

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
 	},
 	"rules": {
 		"indent": [
-			"error",
+			"warn",
 			"tab"
 		],
 		"linebreak-style": [
@@ -21,7 +21,7 @@ module.exports = {
 		],
 		"quotes": [
 			"error",
-			"double"
+			"single"
 		],
 		"semi": [
 			"error",
